a250bff077c93b5bafd3c32824c6b1d008a74eb5
Mathijs Lemmers <mathijs750@gmail.com>
2021-12-15 22:32:32 +0100

    Fix endif

--- vec3.h
+++ vec3.h
@@ -59,8 +59,6 @@ public:
 using point3 = vec3; // 3D point
 using color = vec3;  // RGB color
 
-#endif
-
 // vec3 Utility Functions
 
 inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
@@ -106,3 +104,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif
