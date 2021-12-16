f992ac94083135bc25f5d84667384cf51bb5a4e3
Mathijs Lemmers <mathijs750@gmail.com>
2021-12-16 21:18:03 +0100

    Fix loop bug

--- rust/raytrace/src/main.rs
+++ rust/raytrace/src/main.rs
@@ -6,12 +6,11 @@ fn main() {
     // Render
     print!("P3\n{} {}\n255\n", image_width, image_height);
 
-    for j in 0..image_height - 1 {
-        for i in 0..image_width - 1 {
+    for j in (0..image_height - 1).rev() {
+        for i in 0..image_width {
             let r = (i as f32 / (image_width - 1) as f32) * 255.999;
             let g = (j as f32 / (image_height - 1) as f32) * 255.999;
             let b = (0.25 as f32) * 255.999;
-
 
             print!("{} {} {}\n", r.round(),g.round(),b.round())
         }
