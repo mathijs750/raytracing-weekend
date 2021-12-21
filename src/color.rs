8145b1c1c18f2592828890bbf2f91d9fea42a60f
Mathijs Lemmers <mathijs750@gmail.com>
2021-12-22 00:06:40 +0100

    Fix render function

--- src/color.rs
+++ src/color.rs
@@ -2,6 +2,11 @@ use crate::vec3::*;
 
 impl Color {
     pub fn write_color(self) {
-        print!("{} {} {}\n", self.x, self.y, self.z);
+        print!(
+            "{} {} {}\n",
+            (self.x * 255.999).round(),
+            (self.y * 255.999).round(),
+            (self.z * 255.999).round()
+        );
     }
 }
