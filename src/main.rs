c92ff473de81348df08815a7cc9d34683a9e5fdc
Mathijs Lemmers <mathijs750@gmail.com>
2021-12-22 20:43:36 +0100

    Implement surface normal

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,36 @@ mod vec3;
 
 use crate::vec3::*;
 
-fn hit_sphere(center: Point3, radius: f64, r: ray::Ray) -> bool {
+fn hit_sphere(center: Point3, radius: f64, r: ray::Ray) -> f64 {
     let oc = r.origin - center;
     let a = Vec3::dot(r.direction, r.direction);
     let b = 2.0 * Vec3::dot(oc, r.direction);
     let c = Vec3::dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    return discriminant > 0.0;
+    if discriminant < 0.0 {
+        return -1.0;
+    } else {
+        return (-b - discriminant.sqrt()) / (2.0 * a);
+    }
 }
 
 fn ray_color(r: ray::Ray) -> Color {
-    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
+    let mut t = hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t > 0.0 {
+        let normal = (r.at(t) - Vec3::new(0.0, 0.0, -1.0)).normlised();
+        return Color::new(normal.x + 1.0, normal.y + 1.0, normal.z + 1.0) * 0.5;
     }
 
     let unit_direction = r.direction.normlised();
-    let t = 0.5 * (unit_direction.y + 1.0);
+    t = 0.5 * (unit_direction.y + 1.0);
     return (Vec3::one() * (1.0 - t)) + Color::new(0.5, 0.7, 1.0) * t;
 }
 
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width: i32 = 400;
-    let image_height: i32 = (image_width as f64 / aspect_ratio) as i32;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
 
     //Camera
     let viewport_height = 2.0;
