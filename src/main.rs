dec4222e47e3dac62d3fe2f4633556fb2e2ceec1
Mathijs Lemmers <mathijs750@gmail.com>
2022-01-02 00:40:49 +0100

    Clean main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,33 @@
-mod ray;
-mod vector;
-mod sphere;
+mod camera;
 mod hittable;
 mod hittable_list;
-mod camera;
+mod ray;
 mod render;
+mod sphere;
+mod vector;
 
-use hittable_list::*;
 use camera::Camera;
+use hittable_list::*;
 use render::render;
 
 fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u32 = 1920;
+    const IMAGE_WIDTH: u32 = 400;
     const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
-    const SAMPLES_PER_PIXEL: u32 = 1000;
+    const SAMPLES_PER_PIXEL: u32 = 100;
 
     let camera = Camera::new();
     let world = HittableList::new();
 
-    let buffer = render(&camera, &world, IMAGE_WIDTH, IMAGE_HEIGHT, SAMPLES_PER_PIXEL);
+    let buffer = render(
+        &camera,
+        &world,
+        IMAGE_WIDTH,
+        IMAGE_HEIGHT,
+        SAMPLES_PER_PIXEL,
+    );
 
-    match buffer.save("lambert_big.png") {
+    match buffer.save("lambert_newbook_hemi.png") {
         Err(e) => eprintln!("Error writing file: {}", e),
         Ok(()) => println!("Done."),
     };
